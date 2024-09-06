6d0fd48f9a54aba336ce9d0eec5f6665a0abd621
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-05 22:22:10 -0300

    Create ResetStyle.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import GlobalStyle from './styles/GlobalStyle.jsx'
+import ResetStyle from './styles/ResetStyle.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ResetStyle />
+    <GlobalStyle />
     <App />
   </StrictMode>,
 )
