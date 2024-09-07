3b4aabb3fba4dc952c8190ce00df0a6ecb892108
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-07 16:32:02 -0300

    Update Alert.jsx

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import styled, { keyframes } from 'styled-components'
 
-// Remove the 'interface' keyword
 AlertProps = {
   message: string,
   duration: number,
